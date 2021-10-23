21057e42f5388c78dfbd0e1786926ad2492665a0
tsm rambo <hamzehiyi@outlook.com>
2021-10-23 09:29:38 +0300

    Donwload button added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var button=document.getElementById("btn");
 var row=document.getElementById("row");
+var downloadBtn=document.getElementById("btn1");
 var DateIdValue=1;
 var daysIdValue=20;
+var downloaddVar=0;
 //const dayNames = ["احد", "اثنين", "ثلاثاء", "اربعاء",  "سبت"];
 const dayNames=[];
 dayNames[6]="سبت";
@@ -31,7 +33,7 @@ var data = [
 const getDaysOfWeekBetweenDates = (sDate ,eDate) => {
 var inputValue=document.getElementById("uni_Number").value;
 var daysValue=document.getElementById("days").value;
- input1=document.getElementById("uni_Number").value;
+input1=document.getElementById("uni_Number").value;
 
 var toText = inputValue.toString(); 
 var lastChar = toText.slice(-1); 
@@ -48,6 +50,7 @@ allYear.push([startDate.getDate()] +'/'+    [ startDate.getMonth()+1] + '/'+ [st
 startDate.setDate(startDate.getDate() + 1);
 i++;
   }
+  //sbt-a7d-thlatha frdi
 if(daysValue==1 && isOdd(lastDigit)){
 // 0 =sbt 1=a7d 2=athnen 3=thltha2 4=arb3a2
 document.getElementById(daysIdValue).innerHTML=daysOfWeek[0] + ' - ' + daysOfWeek[1];
@@ -62,7 +65,11 @@ if(daysIdValue>=35 && DateIdValue>=16){
   daysIdValue=20;
   DateIdValue=1;
 }
+downloaddVar=11;
+
 }
+  //sbt-a7d-thlatha zoji
+
 else if(daysValue==1 && isEven(lastDigit)){
 document.getElementById(daysIdValue).innerHTML=daysOfWeek[3];
 document.getElementById(DateIdValue).innerHTML=allYear[3];
@@ -76,7 +83,10 @@ document.getElementById(DateIdValue).innerHTML=allYear[1] + '-' + allYear[0];
   daysIdValue=20;
   DateIdValue=1;
   }
+  downloaddVar=12;
+
   }
+  //sbt athnen arb3a frdi
   else if(daysValue==2 && isOdd(lastDigit)){
     // 0 =sbt 1=a7d 4=athnen 3=thltha2 2=arb3a2
     document.getElementById(daysIdValue).innerHTML=daysOfWeek[0] + ' - ' + daysOfWeek[2];
@@ -91,7 +101,9 @@ document.getElementById(DateIdValue).innerHTML=allYear[1] + '-' + allYear[0];
       daysIdValue=20;
       DateIdValue=1;
     }
+    downloaddVar=21;
     }
+  //sbt athnen arb3a zoji
 
     else if(daysValue==2 && isEven(lastDigit)){
       // 0 =sbt 1=a7d 4=athnen 3=thltha2 2=arb3a2
@@ -109,7 +121,11 @@ document.getElementById(DateIdValue).innerHTML=allYear[1] + '-' + allYear[0];
         daysIdValue=20;
         DateIdValue=1;
       }
+      downloaddVar=22;
+
       }
+
+    //a7d thaltha frdi
   else if(daysValue==3 && isOdd(lastDigit)){
     document.getElementById(daysIdValue).innerHTML=daysOfWeek[1];
     document.getElementById(DateIdValue).innerHTML=allYear[1];
@@ -123,7 +139,11 @@ document.getElementById(DateIdValue).innerHTML=allYear[1] + '-' + allYear[0];
       daysIdValue=20;
       DateIdValue=1;
     }
+    downloaddVar=31;
+
   }
+      //a7d thaltha zoji
+
   else if(daysValue==3 && isEven(lastDigit)){
     document.getElementById(daysIdValue).innerHTML=daysOfWeek[3] ;
     document.getElementById(DateIdValue).innerHTML=allYear[3];
@@ -137,7 +157,10 @@ document.getElementById(DateIdValue).innerHTML=allYear[1] + '-' + allYear[0];
       daysIdValue=20;
       DateIdValue=1;
     }
+    downloaddVar=32;
+
   }
+  //thnen arb3a frdi
   else if(daysValue==4 && isOdd(lastDigit)){
     document.getElementById(daysIdValue).innerHTML=daysOfWeek[2];
     document.getElementById(DateIdValue).innerHTML=allYear[2];
@@ -151,7 +174,11 @@ document.getElementById(DateIdValue).innerHTML=allYear[1] + '-' + allYear[0];
       daysIdValue=20;
       DateIdValue=1;
     }
+    downloaddVar=41;
+
   }
+    //thnen arb3a frdi
+
   else if(daysValue==4 && isEven(lastDigit)){
     document.getElementById(daysIdValue).innerHTML=daysOfWeek[4] ;
     document.getElementById(DateIdValue).innerHTML=allYear[4];
@@ -166,6 +193,8 @@ if(daysIdValue>=35 && DateIdValue>=16){
   daysIdValue=20;
   DateIdValue=1;
 }
+downloaddVar=42;
+
   }
   if(daysIdValue>=35 && DateIdValue>=16){
     daysIdValue=20;
@@ -198,15 +227,57 @@ function isOdd(n) {
   return n % 2 == 1;
 }
 function show(){
-    // Remove any element-specific value, falling back to stylesheets
    
      // input.setCustomValidity('يجب ادخال رقم');
       //input.reportValidity();  
-  
-   
       document.getElementById('cont2').style.display="block"
+      downloadBtn.style.display="block"
     
   }
+
+downloadBtn.onclick=function(){
+  //sbt a7d thltha frdi
+if(downloaddVar==11){
+  window.open("https://i.postimg.cc/Qstfnyrp/image.png?dl=1","_self")
+}
+ //sbt a7d thltha zoji
+ else if(downloaddVar==12){
+  window.open("https://i.postimg.cc/PHK6yk0X/image.png?dl=1","_self")
+ }
+ //sbt athnen arb3a frdi
+ else if(downloaddVar==21){
+  window.open("https://i.postimg.cc/6wmMsf9F/image.png?dl=1","_self")
+ }
+ //sbt athnen arb3a zoji
+ else if(downloaddVar==22){
+  window.open("https://i.postimg.cc/5bQnfQyy/image.png?dl=1","_self")
+ }
+ //a7d thlatha frdi 
+ else if(downloaddVar==31){
+  window.open("https://i.postimg.cc/hDCpt9Vz/image.png?dl=1","_self")
+
+ }
+  //a7d thlatha zoji 
+ else if(downloaddVar==32){
+ window.open("https://i.postimg.cc/wqxwzWRY/image.png?dl=1","_self")
+   }
+//athnen arb3a frdi
+else if(downloaddVar==41){
+  window.open("https://i.postimg.cc/yVyLwqdT/image.png?dl=1","_self")
+ }
+//athnen arb3a zoji
+else if(downloaddVar==42){
+  window.open("https://i.postimg.cc/yVHfQ6Vd/image.png?dl=1","_self")
+ }
+ else {
+   alert("ERORR");
+ }
+}
+
+
+
+
+
 /*button.onclick=function(){
   show();
 };*/
